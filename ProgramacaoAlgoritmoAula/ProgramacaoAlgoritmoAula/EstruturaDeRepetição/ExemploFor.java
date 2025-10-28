1482b53d6d86f98beecbe0ab7ce6dc2cdcb54821
Maria Clara Liossi <mariaclaraliossi@gmail.com>
2025-10-28 20:50:09 -0300

    Salvando

---
+++ ProgramacaoAlgoritmoAula/ProgramacaoAlgoritmoAula/EstruturaDeRepetição/ExemploFor.java
@@ -0,0 +1,15 @@
+package ProgramacaoAlgoritmoAula.ProgramacaoAlgoritmoAula.EstruturaDeRepetição;
+
+import java.util.Scanner;
+
+public class ExemploFor {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        int i;
+
+        for(i=0; i<=9; i++){
+            System.out.println("Etec de Fernandópolis");
+        }
+    }
+}
---
+++ ProgramacaoAlgoritmoAula/ProgramacaoAlgoritmoAula/EstruturaDeRepetição/ExemploFor3.java
@@ -0,0 +1,19 @@
+package ProgramacaoAlgoritmoAula.ProgramacaoAlgoritmoAula.EstruturaDeRepetição;
+
+import java.util.Scanner;
+
+public class ExemploFor3 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        int i, n;
+
+        System.out.println("Digite um número de 1 a 10");
+        n = ler.nextInt();
+
+        for(i = 1; i <=10; i++){
+            System.out.println(n + " x " + i + " = " + (n * i));
+
+        }
+    }
+}
